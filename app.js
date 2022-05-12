5b38cdbded2b1955c11b92fab8a7aaae2185d1b6
Renato Di Giacomo <renato_di_giacomo@hotmail.com>
2022-05-12 18:53:58 -0300

    Change to Hapi server

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const data = require('./data.json')
-
-app.use((req, res, next) => {
-  res.status(200).send(data);
-});
-
-module.exports = app;
